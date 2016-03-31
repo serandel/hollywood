cb9d23e493475a6a58c1724918ba2bef4c73731a
Serandel <serandel@gmail.com>
2016-03-31 20:36:22 +0200

    HollywoodAndroidApplication

---
+++ src/main/java/org/granchi/hollywood/android/HollywoodAndroidApplication.java
@@ -0,0 +1,9 @@
+package org.granchi.hollywood.android;
+
+import android.app.Application;
+
+/**
+ * Android application, powered by Hollywood.
+ */
+public class HollywoodAndroidApplication extends Application {
+}
