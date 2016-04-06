87e646660d56eebfb3334cebf19ef665638b017e
Serandel <serandel@gmail.com>
2016-04-06 21:50:25 +0200

    Make sample app run again, even if it doesn't do anything interesting yet

---
+++ sample-app/src/main/java/org/granchi/whatnow/StubActor.java
@@ -0,0 +1,25 @@
+package org.granchi.whatnow;
+
+import org.granchi.hollywood.Action;
+import org.granchi.hollywood.Actor;
+import org.granchi.hollywood.Model;
+import org.granchi.hollywood.SingleInstanceActorMetadata;
+
+import rx.Observable;
+
+/**
+ * Stub Actor, just for using it so the app runs while building real ones.
+ *
+ * @author serandel
+ */
+public class StubActor implements Actor<SingleInstanceActorMetadata> {
+    @Override
+    public Observable<Action> getActions() {
+        return Observable.never();
+    }
+
+    @Override
+    public void subscribeTo(Observable<Model<SingleInstanceActorMetadata>> models) {
+        // Very well
+    }
+}
--- sample-app/src/main/java/org/granchi/whatnow/WhatNowApplication.java
+++ sample-app/src/main/java/org/granchi/whatnow/WhatNowApplication.java
@@ -2,7 +2,9 @@ package org.granchi.whatnow;
 
 import org.granchi.hollywood.Action;
 import org.granchi.hollywood.Actor;
+import org.granchi.hollywood.Cast;
 import org.granchi.hollywood.Model;
+import org.granchi.hollywood.ModelExceptionHandler;
 import org.granchi.hollywood.SingleInstanceActorMetadata;
 import org.granchi.hollywood.SingleInstanceCast;
 import org.granchi.hollywood.android.HollywoodAndroidApplication;
@@ -10,7 +12,6 @@ import org.granchi.hollywood.android.HollywoodAndroidApplicationCompanion;
 
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Observable;
 import java.util.Set;
 
 /**
@@ -21,7 +22,7 @@ import java.util.Set;
 public class WhatNowApplication extends HollywoodAndroidApplication {
     @Override
     protected HollywoodAndroidApplicationCompanion createHollywoodCompanion() {
-        return new HollywoodAndroidApplicationCompanion(new Model<SingleInstanceActorMetadata>() {
+        Model<SingleInstanceActorMetadata> initialModel = new Model<SingleInstanceActorMetadata>() {
             @Override
             public Model<SingleInstanceActorMetadata> actUpon(Action action) {
                 return this;
@@ -29,20 +30,24 @@ public class WhatNowApplication extends HollywoodAndroidApplication {
 
             @Override
             public Set<SingleInstanceActorMetadata> getActors() {
-                return new HashSet<>(Collections.singletonList(new SingleInstanceActorMetadata("org.granchi.whatnow.MockActor")));
+                return new HashSet<>(Collections.singletonList(new SingleInstanceActorMetadata(StubActor.class.getName())));
             }
-        }, metadata -> {
-            return new SingleInstanceCast((Actor.Factory<SingleInstanceActorMetadata>) metadata1 -> {
-                try {
-                    return (Actor<SingleInstanceActorMetadata>) Class.forName(metadata1.getActorClass()).getConstructor().newInstance();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }, Observable < Model < SingleInstanceActorMetadata >>.empty());
-        }, (model, action, exception) -> {
+        };
+
+        Cast.Factory<SingleInstanceActorMetadata> castFactory = models -> new SingleInstanceCast(metadata -> {
+            try {
+                return (Actor<SingleInstanceActorMetadata>) Class.forName(metadata.getActorClass()).getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }, models);
+
+        ModelExceptionHandler<SingleInstanceActorMetadata> exceptionHandler = (model, action, exception) -> {
             showError("Exception");
             return model;
-        });
+        };
+
+        return new HollywoodAndroidApplicationCompanion(initialModel, castFactory, exceptionHandler);
     }
 
     @Override
