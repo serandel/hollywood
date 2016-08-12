110995fde90924c758c418f9d617d921edc6fd07
Serandel <serandel@gmail.com>
2016-08-12 19:20:31 +0200

    Fix compilation error

--- android/src/main/java/org/granchi/hollywood/android/HollywoodAndroidApplicationCompanion.java
+++ android/src/main/java/org/granchi/hollywood/android/HollywoodAndroidApplicationCompanion.java
@@ -1,8 +1,11 @@
 package org.granchi.hollywood.android;
 
+import org.granchi.hollywood.Actor;
 import org.granchi.hollywood.HollywoodApplication;
 import org.granchi.hollywood.Model;
 import org.granchi.hollywood.ModelExceptionHandler;
+
+import java.util.Collection;
 
 import timber.log.Timber;
 
@@ -21,13 +24,15 @@ public class HollywoodAndroidApplicationCompanion extends
      *
      * @param app              HollywoodAndroidApplication, can't be null
      * @param initialModel     initial Model, can't be null
+     * @param actors           Actors of the application, can't be null or empty
      * @param exceptionHandler handler for Exceptions during Model.actUpon, can be null to just end
      *                         the app if it
      */
     public HollywoodAndroidApplicationCompanion(HollywoodAndroidApplication app,
                                                 Model initialModel,
+                                                Collection<Actor> actors,
                                                 ModelExceptionHandler exceptionHandler) {
-        super(initialModel, exceptionHandler);
+        super(initialModel, actors, exceptionHandler);
 
         if (app == null) {
             throw new NullPointerException();
